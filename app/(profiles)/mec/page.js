9676758311a995049555cfadfde380e3a73d24b6
Vulgo Mec <vulgomec@icloud.com>
2024-05-13 20:32:36 -0300

    updated

--- app/(profiles)/mec/page.js
+++ app/(profiles)/mec/page.js
@@ -25,24 +25,24 @@ export default function Mec() {
           >
             clique aqui
           </motion.div>
-        ) : null}
+        ) : (
+          <iframe
+            className="absolute h-[200%] w-[200%] top-[-90%] left-[-50%] lg:top-[-80%] lg:left-[-25%]"
+            src="https://www.youtube-nocookie.com/embed/_v7BThrUPB0?si=kJ-jeaYoH9ANjPm7&amp;autoplay=1&amp;controls=0"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          />
+        )}
       </AnimatePresence>
 
-      {isProtected ? null : (
-        <iframe
-          className="absolute h-[200%] w-[200%] top-[-90%] left-[-50%] lg:top-[-80%] lg:left-[-25%]"
-          src="https://www.youtube-nocookie.com/embed/R0ykLlhg0AQ?si=kJ-jeaYoH9ANjPm7&amp;autoplay=1&amp;controls=0"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        />
-      )}
-
-      <div className="absolute inset-0 flex flex-col gap-4 p-4 lg:px-[12rem] lg:py-[4.20rem] overflow-auto bg-black bg-opacity-75 backdrop-blur-sm lg:backdrop-blur backdrop-contrast-200">
+      <div className="absolute inset-0 flex flex-col gap-4 p-4 lg:px-[12rem] lg:py-[4.20rem] overflow-auto bg-black bg-opacity-75 backdrop-blur-sm lg:backdrop-blur backdrop-grayscale backdrop-contrast-200 backdrop-brightness-200">
         <header>
           <div className="flex gap-4">
-            <img className="w-[124px] h-[124px] rounded-2xl" src="/mec/profile.jpg" />
+            <div className="w-[124px] h-[124px]">
+              <img className="h-full w-full rounded-2xl " src="/mec/profile.gif" />
+            </div>
 
             <div className="flex flex-col gap-4 my-auto">
               <h1 className="text-2xl font-bold leading-none">vulgo mec</h1>
